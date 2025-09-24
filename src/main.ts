a4396f3bdf37ce62015780ab281dddb10d68bed4
Marshall Wice <mhwice@protonmail.com>
2025-09-24 16:27:03 -0700

    add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,19 @@ async function bootstrap() {
     // essentially this gives us the result of zod.parse
     transform: true
   }));
+
+  // swagger config
+  const config = new DocumentBuilder()
+    .setTitle("Blog")
+    .setDescription("Use the base API URL of http://localhost:3000")
+    .setTermsOfService("http://localhost:3000/tos")
+    .setLicense("MIT License", "https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt")
+    .addServer("http://localhost:3000")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,13 +12,39 @@ import { PatchUserDto } from './dtos/patch-user.dto';
 
 import { UsersService } from './providers/users.service';
 
+// Swagger
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+
 @Controller('users')
+@ApiTags("Users")
 export class UsersController {
 
   // Injecting user service
   constructor(private readonly usersService: UsersService) {}
 
   @Get("{/:id}")
+  @ApiOperation({
+    summary: "Fetches a list of registed users on the application"
+  })
+  @ApiResponse({
+    status: 200,
+    description: "Users fetched successfully based on the query"
+  })
+  @ApiQuery({
+    name: "limit",
+    type: "number",
+    required: false,
+    description: "The number of entries returned per query",
+    example: 10
+  })
+  @ApiQuery({
+    name: "page",
+    type: "number",
+    required: false,
+    description: "The position of the page number that you want the API to return",
+    example: 1
+  })
   public getUsers(
     @Param() getUserParamDto: GetUsersParamDto,
     @Query("limit", new DefaultValuePipe(10), ParseIntPipe) limit: number,
